212f4265b466d6e095b0eb712b8a822cd0587297
Jordan <jtrask@gmail.com>
2022-09-28 11:49:20 -0400

    Fixed some issues with proxmox-report.sh

--- mikrotik-dhcp-leases.sh
+++ mikrotik-dhcp-leases.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-HOST="192.168.80.1"
-USER="admin"
-SSH="ssh $USER@$HOST "
+SCRIPT_DIR=$( basename -- "$0"; )
+CONFIG="$SCRIPT_DIR/env.config"
+DATE=`date`
+
+SSH="ssh $MIKROTIK_USER@$MIKROTIK_HOST "
 SSH_OUTPUT=$(eval $SSH "/ip/dhcp-server/lease print")
 
 echo "$SSH_OUTPUT"
--- proxmox-report.sh
+++ proxmox-report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SCRIPT_DIR=$( basename -- "$0"; )
-CONFIG="$SCRIPT_DIR/env.config"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONFIG="$SCRIPT_DIR/env.cfg"
 DATE=`date`
 
 # -- check for env.cnf
@@ -11,13 +11,16 @@ else
 	exit 1
 fi
 
-# -- scmd sshe command
+# -- ssh command
 scmd () {
-	ssh $PROXMOX_USER@$PROXMOX_HOST "$@"
+	eval "ssh ${PROXMOX_USER}@${PROXMOX_HOST} $@"
 }
 
+PROXMOX_UPTIME=$(scmd uptime)
+PROXMOX_HOSTNAME=$(scmd hostname -af)
+
 # -- main loop
-QM_LIST=$(ssh root@$HOST "qm list")
+QM_LIST=$(scmd "qm list")
 QM_IDS=$(echo "$QM_LIST" | awk {' print $1 '} | grep -v 'VMID')
 QM_NAMES=$(echo "$QM_LIST" | awk {' print $2 '} | grep -v 'NAME')
 QM_STATUS=$(echo "$QM_LIST" | awk {' print $3 '} | grep -v 'STATUS')
@@ -28,15 +31,14 @@ readarray -t QM_STATUS <<< "$QM_STATUS"
 
 # New Status
 echo '<div id="pm_status" style="color: white;"><pre>'
+echo "Host: $PROXMOX_HOSTNAME"
+echo "Uptime: $PROXMOX_UPTIME"
 echo "ID     Name                      Status       Network"
-#printf '%-10s %-10s %-10s %-10s'
 i=0
 for VM_ID in ${QM_IDS[@]}; do
 	QM_NETWORK=$(scmd "qm guest cmd $VM_ID network-get-interfaces 2>&1" )	
 	#	echo " -- Network Interfaces
 	# qm guest cmd 105 network-get-interfaces
-#	echo "${QM_IDS[$i]}"
-#	echo ""
 	printf "%-6s %-25s %-12s %s\n" ${QM_IDS[$i]} ${QM_NAMES[$i]} ${QM_STATUS[$i]} "$QM_NETWORK"
 	((i=i+1))
 done
