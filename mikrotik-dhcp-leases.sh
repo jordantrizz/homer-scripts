823a2e4ac0e3581ca83d0e976c6689bad96ce379
Jordan <jtrask@gmail.com>
2022-09-28 11:52:20 -0400

    Updated mikrotik-dhcp-leases.sh to work properly

--- mikrotik-dhcp-leases.sh
+++ mikrotik-dhcp-leases.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-SCRIPT_DIR=$( basename -- "$0"; )
-CONFIG="$SCRIPT_DIR/env.config"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+CONFIG="$SCRIPT_DIR/env.cfg"
 DATE=`date`
 
-SSH="ssh $MIKROTIK_USER@$MIKROTIK_HOST "
-SSH_OUTPUT=$(eval $SSH "/ip/dhcp-server/lease print")
+# -- check for env.cnf
+if [[ -a $CONFIG ]]; then
+    source $CONFIG
+else
+    echo "Error: can't find config at $CONFIG"
+    exit 1
+fi
 
+# -- ssh command
+scmd () {
+    eval "ssh ${MIKROTIK_USER}@${MIKROTIK_HOST} $@"
+}
+
+SSH_OUTPUT=$(scmd /ip/dhcp-server/lease print)
+
+
+echo '<div id="pm_status" style="color: white;"><pre>'
 echo "$SSH_OUTPUT"
+echo ""
+echo "Date: $DATE"
+echo '</pre></div>'
